5a784cf134c561cdbdcb419fb8c4cc7dacfc24e0
AntonioSpi <112627907+AntonioSpi@users.noreply.github.com>
2023-11-22 10:15:13 +0100

    några dörrar funkar

--- Assets/Scripts/ToggleTextField.cs
+++ Assets/Scripts/ToggleTextField.cs
@@ -21,8 +21,8 @@ public class ToggleTextField : MonoBehaviour
     void Update()
     {
         float dist = Vector3.Distance(door.position, transform.position);
+        Debug.Log(dist);
         InterText.SetActive(false);
-
         if (dist < 2)
         {
             InterText.SetActive(true);
